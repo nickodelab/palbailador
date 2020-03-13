be6e89aea72f1cf54fa47413d1cda0cb023c8dae
nico <nicolas.martin.acosta@gmail.com>
2020-03-13 19:15:12 +0100

    adding --bail to husky hook

--- backend/src/logic/index.spec.js
+++ backend/src/logic/index.spec.js
@@ -67,7 +67,7 @@ describe('LOGIC', () => {
       it('NOK - should thrown an error on non valid email', async () => {
         await expect(
           logic.registerUser({ ...user, email: 'failEmail@' })
-        ).to.be.rejectedWith(TypeError, 'non val id email')
+        ).to.be.rejectedWith(TypeError, 'non valid email')
       })
 
       it('NOK - should thrown an error on email = ""', async () => {
